289ca21657a65a3f146bb0573db5cfe2cc1b4068
cleuristongato <108682519+cleuristongato@users.noreply.github.com>
2023-09-08 12:21:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const PORT = 3333
+//Importar o módulo conn para as operações com o banco
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+//Middleware para arquivos estáticos
+
+
+app.get('/', (request, response)=>{
+  return response.render('home')
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
+
+Conn.connect(function(erro){
+  if(error){
+    console.log(erro)
+    return
+  }
+  console.log('MYSQL CONECTEDO')
+  app.listen(PORT, ()=>{
+    console.log(`servidor rodando na porta ${PORT}`)
+  })
+})
